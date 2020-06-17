0abe07ab8cc3c2d66cb54237ab1d9e11b516b527
iFlameing <ialokkumarsingh0@gmail.com>
2020-06-17 20:31:44 +0530

    initial commit

---
+++ src/addons/volto-form-builder/src/components/Input/InputSidebar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+import { FormattedMessage, injectIntl, defineMessages } from 'react-intl';
+import { TextWidget, CheckboxWidget } from '@plone/volto/components';
+
+const messages = defineMessages({
+  label: {
+    id: 'Label',
+    defaultMessage: 'Label',
+  },
+  required: {
+    id: 'Required',
+    defaultMessage: 'Required',
+  },
+});
+
+const InputSidebar = (props) => {
+  const value = props.data.input;
+  return (
+    <Segment.Group raised>
+      <header className="header pulled">
+        <h2>
+          <FormattedMessage id="Input" defaultMessage="Input" />
+        </h2>
+      </header>
+      <Segment className="form sidebar-image-data">
+        <TextWidget
+          id="external"
+          title={props.intl.formatMessage(messages.label)}
+          required={true}
+          value={value}
+          onChange={(e, v) => {
+            props.onChangeBlock(props.block, {
+              ...props.data,
+              input: v,
+            });
+          }}
+        />
+      </Segment>
+      <Segment className="form sidebar-image-data">
+        <CheckboxWidget
+          id="external"
+          title={props.intl.formatMessage(messages.required)}
+          value={props.data.required}
+          onChange={(e, v) => {
+            props.onChangeBlock(props.block, {
+              ...props.data,
+              required: v,
+            });
+          }}
+        />
+      </Segment>
+    </Segment.Group>
+  );
+};
+
+export default injectIntl(InputSidebar);
