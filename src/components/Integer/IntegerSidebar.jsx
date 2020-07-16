bf871e9fd1d26a03c8e5ca3fed40e7343d54f19b
Alok Kumar <ialokkumarsingh0@gmail.com>
2020-07-16 20:09:34 +0530

    final implementation (#31)

---
+++ src/components/Integer/IntegerSidebar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Segment } from "semantic-ui-react";
+import { FormattedMessage, injectIntl, defineMessages } from "react-intl";
+import { TextWidget } from "@plone/volto/components";
+
+const messages = defineMessages({
+  label: {
+    id: "Label",
+    defaultMessage: "Label",
+  },
+});
+
+const IntegerSidebar = (props) => {
+  const value = props.data.label;
+  return (
+    <Segment.Group raised>
+      <header className="header pulled">
+        <h2>
+          <FormattedMessage id="Integer" defaultMessage="Integer" />
+        </h2>
+      </header>
+      <Segment className="form sidebar-image-data">
+        <TextWidget
+          id="external"
+          title={props.intl.formatMessage(messages.label)}
+          required={true}
+          value={value}
+          onChange={(e, v) => {
+            props.onChangeBlock(props.block, {
+              ...props.data,
+              label: v,
+            });
+          }}
+        />
+      </Segment>
+    </Segment.Group>
+  );
+};
+
+export default injectIntl(IntegerSidebar);
--- src/components/Integer/View.jsx
+++ src/components/Integer/View.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import TextIntegerNumber from "../Widgets/TextNumber";
+import { Form } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { setFormbuilderInputValue } from "../../actions";
 
-const View = () => {
+const IntegerView = ({
+  data,
+  formbuilder,
+  id,
+  path,
+  setFormbuilderInputValue,
+  block,
+  ...rest
+}) => {
+  const blockid = id;
   return (
-    <div>
-      <h1>This is view for integer</h1>
-    </div>
+    <>
+      <Form>
+        <TextIntegerNumber
+          id="integer-edit"
+          title={data.label}
+          required={true}
+          value={formbuilder[path]?.[blockid] || ""}
+          onChange={(id, value) => {
+            setFormbuilderInputValue(path, blockid, value);
+          }}
+          wrapped
+        />
+      </Form>
+    </>
   );
 };
 
-export default View;
+export default connect(
+  (state, props) => {
+    return {
+      formbuilder: state.formbuilder,
+    };
+  },
+  {
+    setFormbuilderInputValue,
+  }
+)(IntegerView);
